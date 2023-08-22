2db3a95d937527ad848f1b4610e1b4eab59be1c7
Roma <129154115+wascrashed@users.noreply.github.com>
2023-08-22 11:36:57 +0300

    NewsLetter

--- app/Http/Controllers/NewsLetterController.php
+++ app/Http/Controllers/NewsLetterController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendNewsletter;
+use App\Models\Subscribers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class NewsLetterController extends Controller
 {
     public function store(Request $request)
     {
         $news = $request->all();
+        ///return $news;
+        $subs = Subscribers::where('status', 1)->get();
+        foreach ($subs as $sub) {
+            dispatch(New SendNewsletter($sub->email, $news));
+        }
+        return response()->json([
+            'msg' => 'SUCCESS',
+        ]);
     }
 }
