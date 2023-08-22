3f9e0dfbcbd907d5cbdf41bf3ebcd9fa24f33670
Tabibulaev <tabibulaev55@gmail.com>
2023-08-22 14:14:57 +0500

    add auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('email_provider');
             $table->string('email_username');
             $table->string('email_password');
+            $table->string('email_provider');
             $table->rememberToken();
             $table->timestamps();
         });
