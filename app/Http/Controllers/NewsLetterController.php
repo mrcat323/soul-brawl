d86b0973ba1eed9dddf0c6a88023279c20fce1b8
Roma <129154115+wascrashed@users.noreply.github.com>
2023-08-22 08:28:36 +0300

    NewsLetter

---
+++ app/Http/Controllers/NewsLetterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsLetterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $news = $request->all();
+        dd($news);
+    }
+}
--- routes/platform.php
+++ routes/platform.php
@@ -27,10 +27,6 @@ Route::screen('/main', PlatformScreen::class)
 
 Route::screen('subscribers', \App\Orchid\Screens\Subscribers\SubscribersScreen::class)
     ->name('platform.subscribers');
-Route::screen('newsletter', \App\Orchid\Screens\NewsLetter\NewsLetterScreen::class)
-    ->name('platform.newsletter');
-Route::screen('newsletter-add', \App\Orchid\Screens\NewsLetter\AddNewsLetterScreen::class)
-    ->name('platform.newsletter.add');
 
 // Platform > Profile
 Route::screen('profile', UserProfileScreen::class)
